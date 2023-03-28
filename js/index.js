b39b18d6fe8c48e3a5c41e114e1e7178aa5e2459
ArafatAkashAkku <arafatakash5@gmail.com>
2023-03-28 22:13:09 +0600

    Navbar Responsive

--- js/index.js
+++ js/index.js
@@ -1,10 +1,39 @@
 const navigationBar = document.getElementById('navigation-bar');
 
-window.onscroll=()=>{
-    if(scrollY>100){
-        navigationBar.style.backgroundColor="black";
+window.onscroll = () => {
+    if (scrollY > 100) {
+        navigationBar.style.backgroundColor = "black";
     }
-    else{
-        navigationBar.style.backgroundColor="transparent";
+    else {
+        navigationBar.style.backgroundColor = "transparent";
     }
+}
+
+
+const phoneBar = document.getElementById('phone-bar');
+const phoneNav = document.querySelector('.navbar');
+
+phoneBar.onclick = () => {
+    phoneNav.classList.toggle("active")
+
+    if (phoneNav.classList.contains("active")) {
+        phoneBar.classList.replace("fa-bars", "fa-xmark")
+    } else {
+        phoneBar.classList.replace("fa-xmark", "fa-bars")
+
+    }
+}
+
+const navLinks = document.querySelectorAll('header .navbar ul li')
+
+navLinks.forEach((element)=>{
+    element.onclick=()=>{
+    phoneNav.classList.remove("active")
+    phoneBar.classList.replace("fa-xmark", "fa-bars")
+    }
+})
+
+document.oncontextmenu=(element)=>{
+    element.preventDefault();
+
 }
